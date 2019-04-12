c7c1fffa6ba70ac76a18d2398c86ffcd497955de
nima58 <nima.mazdak@yahoo.com>
2019-04-12 10:00:50 +0200

    Update HelloController.java

--- src/main/java/com/example/springbootopenshiftdemo/HelloController.java
+++ src/main/java/com/example/springbootopenshiftdemo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World from OpenShift .....!";
+        return "Hello World from OpenShift updated .....!";
     }
 }
